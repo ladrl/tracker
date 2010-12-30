a73b23b3ed4f49ee8a35e224a62ac1fb0f77e49e
Lukas Lädrach <lukasladrach@Lukas-Ladrachs-MacBook-Pro-2.local>
2010-12-30 22:44:33 +0100

     [ADD] Simple view of all books

--- ui/webUI/src/main/scala/scalatoys/tracker/webUI/snippet/Libraries.scala
+++ ui/webUI/src/main/scala/scalatoys/tracker/webUI/snippet/Libraries.scala
@@ -4,8 +4,13 @@ import net.liftweb.util.Props
 import scalatoys.tracker._
 import scalatoys.tracker.impl.mongodb._
 
-import net.liftweb.common._
-import net.liftweb.common.Box._
+import net.liftweb.common.{Full,Box,Empty,Failure,Loggable}
+import net.liftweb.util.Helpers._
+import net.liftweb.http.{S,StatefulSnippet,SHtml}
+import Box._
+
+
+import scala.xml.NodeSeq
 
 class Libraries {
 
@@ -21,14 +26,24 @@ class Libraries {
   val me = "webUser"
 	val libName = "webLib"
 
+  def listBooks(xhtml: NodeSeq): NodeSeq = {
+	val library = Library(libName) ?~! "Library %s not found".format(libName)
+	(for(l <- library)
+		yield showBooks(l, xhtml)).get
+  }
 
-  def listBooks = <span> {
-    val library = Library(libName) ?~! "Library %s not found".format(libName)
-    library.map{_.catalogue.query.map{b =>
-      <br/> + "Pages: " + b.pages + " FrontPage: " + b.frontPage
+  def showBooks(library: Library, xhtml: NodeSeq) = library.catalogue.query.flatMap { showBook(_, xhtml) }
 
-    }.reduceRight(_ + <br/> + _ )}
-
-  }  </span>
+  def showBook(book: Book, xhtml: NodeSeq): NodeSeq = {
+	bind("book", xhtml, 
+		"frontPage" -> book.frontPage.flatMap{ t =>
+			bind("page", chooseTemplate("book", "frontPage", xhtml), "key" -> t._1, "value" -> t._2.content)
+		}.toSeq,
+		"pages" -> book.pages.flatMap { p =>
+			println(chooseTemplate("book", "pages", xhtml))
+			bind("page", chooseTemplate("book", "pages", xhtml), "page" -> p.content)
+		}.toSeq
+	)
+  }
 }
 
